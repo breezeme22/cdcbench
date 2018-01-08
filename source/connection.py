b61a2f50cfd2fe8d232c760d2623503a304fb106
sangcheolpark <sangcheol.park@idatabank.com>
2018-01-08 16:10:55 +0900

    db connection 정보 config 파일로 분리

--- source/connection.py
+++ source/connection.py
@@ -2,8 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+rdbms = config["db_connection"]["rdbms"]
+user_id = config["db_connection"]["user_id"]
+user_password = config["db_connection"]["user_password"]
+host_name = config["db_connection"]["host_name"]
+port = config["db_connection"]["port"]
+instance_name = config["db_connection"]["instance_name"]
+
+conn_info = rdbms + "://" + user_id + ":" + user_password + "@" + host_name + ":" + port + "/" + instance_name
+
 # DB Connection 정보 입력.
-engine = create_engine('oracle://testsc:testsc@172.16.2.44:1521/ORA11', convert_unicode=True)
+engine = create_engine(conn_info, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
