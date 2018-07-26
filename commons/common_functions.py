50615da406f0c65b321208d3d82d093a681f273f
sangcheolpark <sangcheol.park@idatabank.com>
2018-07-26 17:25:22 +0900

    공통 함수 구현 모듈 추가

---
+++ commons/common_functions.py
@@ -0,0 +1,30 @@
+import csv
+import os
+
+
+# Selection Function
+def selection_return(print_text):
+    user_input = input(print_text)
+
+    if len(user_input) == 0 or user_input is None:
+        user_input = "N"
+
+    user_input = user_input.strip().upper()
+
+    if user_input == "Y":
+        return True
+    elif user_input == "N":
+        return False
+    else:
+        return None
+
+
+# Data file read
+def get_data(file_name):
+
+    f = open(file_name, "r", encoding="utf-8")
+    reader = csv.reader(f)
+    reader = list(reader)
+    f.close()
+
+    return reader
