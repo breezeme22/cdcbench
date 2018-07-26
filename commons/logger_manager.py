34c880be79a5a220908c05a0eb5f85455d8d19ed
sangcheolpark <sangcheol.park@idatabank.com>
2018-07-26 17:30:34 +0900

    staticmethod 구조로 변경

--- commons/logger_manager.py
+++ commons/logger_manager.py
@@ -1,22 +1,20 @@
 import logging
-import os
 
 
 class LoggerManager:
-    def __init__(self, module_name, log_level=logging.INFO):
 
-        self.level = log_level
-        self.module_name = module_name
-
-    def get_logger(self):
+    @staticmethod
+    def get_logger(module_name, log_level):
 
         formatter = logging.Formatter("%(asctime)s [%(module)s][%(levelname)s] %(message)s")
 
-        file_handler = logging.FileHandler("./cdcbench.log")
-
+        file_handler = logging.FileHandler("cdcbench.log")
         file_handler.setFormatter(formatter)
-        logger = logging.getLogger(self.module_name)
-        logger.setLevel(self.level)
+
+        logger = logging.getLogger(module_name)
+
+        logger.setLevel(log_level)
+
         logger.addHandler(file_handler)
 
         return logger
