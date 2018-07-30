d462ed2457a9ffde55db15c4c72f08fafb645d09
sangcheolpark <sangcheol.park@idatabank.com>
2018-07-30 09:29:54 +0900

    get_engine, get_db_session 함수 제거, db connection test 및 예외처리 추가

--- commons/connection_manager.py
+++ commons/connection_manager.py
@@ -1,36 +1,41 @@
 from commons.logger_manager import LoggerManager
 from commons.config_manager import ConfigManager
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, sql
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import DatabaseError
+
 
 MapperBase = declarative_base()
 
 
 class ConnectionManager:
 
-    logger = None
-
     def __init__(self, connection_string):
 
-        self.config = ConfigManager.get_config()
-        self.logger = LoggerManager.get_logger(__name__, self.config .log_level)
+        try:
+            self.config = ConfigManager.get_config()
+            self.logger = LoggerManager.get_logger(__name__, self.config .log_level)
 
-        self.logger.debug("Connection String: " + connection_string)
+            self.logger.debug("Connection String: " + connection_string)
 
-        self.logger.info("Create Engine")
-        self.engine = create_engine(connection_string, convert_unicode=True)
-        self.logger.debug("Engine Check")
+            self.logger.info("Create Engine")
+            self.engine = create_engine(connection_string, convert_unicode=True)
 
-        self.logger.info("Create DB Session")
-        self.db_session = scoped_session(sessionmaker(autocommit=False, bind=self.engine))
-        self.logger.debug("Engine Check")
+            self.logger.info("Create DB Session")
+            self.db_session = scoped_session(sessionmaker(autocommit=False, bind=self.engine))
 
-        MapperBase.query = self.db_session.query_property()
+            query = sql.select([1])
+            self.engine.execute(query)
+            self.logger.info("DB Connection Test: Success")
 
-    def get_engine(self):
-        return self.engine
+            MapperBase.query = self.db_session.query_property()
 
-    def get_db_session(self):
-        return self.db_session
+        except DatabaseError as dberr:
+            print()
+            self.logger.error("DB Connection Test: Fail")
+            self.logger.error(dberr.args[0])
+            self.logger.error(dberr.statement)
+            self.logger.error(dberr.params)
+            raise
