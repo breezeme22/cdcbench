e1a9063708dd134d6c562ce8d58a022f359c0bb6
sangcheolpark <sangcheol.park@idatabank.com>
2018-07-30 09:29:00 +0900

    elapsed time, commit 메시지 출력 함수 추가

--- commons/common_functions.py
+++ commons/common_functions.py
@@ -2,7 +2,7 @@ import csv
 
 
 # Selection Function
-def selection_return(print_text):
+def get_selection(print_text):
     user_input = input(print_text)
 
     if len(user_input) == 0 or user_input is None:
@@ -31,3 +31,21 @@ def get_data(file_name):
 
     except FileNotFoundError:
         raise FileNotFoundError("Data file ({}) does not exist.".format(file_name))
+
+
+# return Elapse time
+def get_elapsed_time_msg(start_time, end_time):
+
+    try:
+        s_time = float(start_time)
+        e_time = float(end_time)
+
+        return "Elapsed Time: {:.2f} Sec.".format(max(s_time, e_time) - min(s_time, e_time))
+
+    except ValueError as err:
+        raise ValueError(err)
+
+
+# return Commit Message
+def get_commit_msg(commit_value):
+    return "{} Commit is occurred".format(commit_value)
