55ef7ef1137e78806bb47600d4401b6bbf747ed7
sangcheolpark <sangcheol.park@idatabank.com>
2018-08-08 20:28:55 +0900

    LONG RAW, UROWID 타입 추가

--- mappers/oracle_custom_types.py
+++ mappers/oracle_custom_types.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, Binary, LargeBinary, types as sqltypes
 from sqlalchemy.ext.compiler import compiles
 
 
@@ -10,3 +10,24 @@ class VARCHAR2Byte(String):
 def complie_varchar2_byte(type_, compiler, **kw):
     type_len = type_.length
     return "VARCHAR2(%i BYTE)" % type_len
+
+
+class LONGRAW(LargeBinary):
+    pass
+
+
+@compiles(LONGRAW)
+def complie_long_raw(type_, complier, **kw):
+    return "LONG RAW"
+
+
+class UROWID(sqltypes.TypeEngine):
+    pass
+
+
+@compiles(UROWID)
+def complie_urowid(type_, complier, **kw):
+    return "UROWID"
+
+
+
