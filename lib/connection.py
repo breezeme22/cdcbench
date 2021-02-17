1602611e0bc24c0c7470ccfe005b5bdec734224f
박 상철 <sangcheol.park@iarkdata.com>
2021-02-17 23:44:46 +0900

    ConnectionManager SA 지원/미지원 분리

--- lib/connection.py
+++ lib/connection.py
@@ -32,14 +32,7 @@ class ConnectionManager:
             TIBERO: "com.tmax.tibero.jdbc.TbDriver"
         }
 
-        self.dbms = conn_info.dbms
         self.driver = dialect_driver[conn_info.dbms]
-        self.host = conn_info.host
-        self.port = conn_info.port
-        self.dbname = conn_info.dbname
-        self.username = conn_info.username
-        self.password = conn_info.password
-        self.schema = conn_info.schema
 
         # if conn_info.dbms in sa_unsupported_dbms:
         #     self.engine = None
@@ -49,55 +42,78 @@ class ConnectionManager:
         self.Session = None
 
         # else:
+        conn_str: str
         if conn_info.dbms == ORACLE:
             import cx_Oracle
-            dsn = cx_Oracle.makedsn(self.host, self.port, service_name=self.dbname)
-            conn_string = f"{self.driver}://{self.username}:{self.password}@{dsn}"
+            dsn = cx_Oracle.makedsn(conn_info.host, conn_info.port, service_name=conn_info.dbname)
+            conn_str = f"{self.driver}://{conn_info.username}:{conn_info.password}@{dsn}"
         elif conn_info.dbms == MYSQL:
-            conn_string = f"{self.driver}://{self.username}:{self.password}@{self.host}:{self.port}/" \
-                          f"{self.dbname}?charset=utf8mb4"
+            conn_str = (f"{self.driver}://{conn_info.username}:{conn_info.password}@{conn_info.host}:{conn_info.port}/"
+                        f"{conn_info.dbname}?charset=utf8mb4")
         elif conn_info.dbms == SQLSERVER:
-            conn_string = f"{self.driver}://{self.username}:{self.password}@{self.host}:{self.port}/" \
-                          f"{self.dbname}?driver=SQL+SERVER"
-        else:
-            conn_string = f"{self.driver}://{self.username}:{self.password}@{self.host}:{self.port}/" \
-                          f"{self.dbname}"
+            conn_str = (f"{self.driver}://{conn_info.username}:{conn_info.password}@{conn_info.host}:{conn_info.port}/"
+                        f"{conn_info.dbname}?driver=SQL+SERVER")
+        else:   # PostgreSQL
+            conn_str = (f"{self.driver}://{conn_info.username}:{conn_info.password}@{conn_info.host}:{conn_info.port}/"
+                        f"{conn_info.dbname}")
 
-        self.logger.debug(f"Connection String: {conn_string}")
+        self.logger.debug(f"Connection String: {conn_str}")
 
-        self.engine = create_engine(conn_string, convert_unicode=True, max_identifier_length=128)
+        self.engine = create_engine(conn_str, convert_unicode=True, max_identifier_length=128)
 
         self.Session = scoped_session(sessionmaker(autocommit=False, bind=self.engine))
 
-    def sa_unsupported_get_connection(self) -> jaydebeapi.Connection:
-        """
-        SQLAlchemy 미지원 DBMS에서 connection을 맺을 때 사용
-        :return: dbms별 connection
-        """
 
-        jar_file_name = {
-            CUBRID: "JDBC-10.2-latest-cubrid.jar",
-            TIBERO: "tibero6-jdbc.jar"
+class SAUnsupportedConnectionManager:
+
+    def __init__(self, conn_info: DatabaseConfig):
+
+        self.logger = LoggerManager.get_logger(__name__)
+
+        connection_string_value_check(conn_info)
+
+        self.conn_info = conn_info
+
+        dialect_driver = {
+            TIBERO: "com.tmax.tibero.jdbc.TbDriver",
+            CUBRID: "cubrid.jdbc.driver.CUBRIDDriver"
         }
 
-        urls = {
-            CUBRID: f"jdbc:CUBRID:{self.host}:{self.port}:{self.dbname}:public::?charSet=utf-8",
-            TIBERO: f"jdbc:tibero:thin:@{self.host}:{self.port}:{self.dbname}"
+        self.driver = dialect_driver[conn_info.dbms]
+        self.dbms = conn_info.dbms
+        self.host = conn_info.host
+        self.port = conn_info.port
+        self.dbname = conn_info.dbname
+        self.username = conn_info.username
+        self.password = conn_info.password
+
+        self.conn_str: str
+        if self.dbms == TIBERO:
+            self.conn_str = f"jdbc:tibero:thin:@{self.host}:{self.port}:{self.dbname}"
+        else:   # CUBRID
+            self.conn_str = f"jdbc:CUBRID:{self.host}:{self.port}:{self.dbname}:public::?charSet=utf-8"
+
+    def connect(self) -> jaydebeapi.Connection:
+
+        dialect_driver_files = {
+            TIBERO: "tibero6-jdbc.jar",
+            CUBRID: "JDBC-10.2-latest-cubrid.jar"
         }
+
+        self.logger.debug(
+            f"driver={self.driver}, "
+            f"url={self.conn_str}, "
+            f"user={[self.username, self.password]}, "
+            f"jar={os.path.join('driver', self.dbms.lower(), dialect_driver_files[self.dbms])}"
+        )
 
         try:
-            self.logger.debug(
-                f"driver={self.driver},"
-                f"url={urls[self.dbms]}, "
-                f"user={[self.username, self.password]}, "
-                f"jar={os.path.join('lib', '../driver', self.dbms.lower(), jar_file_name[self.dbms])}"
-            )
 
             return jaydebeapi.connect(
                 self.driver,
-                urls[self.dbms],
+                self.conn_str,
                 [self.username, self.password],
-                os.path.join("driver", self.dbms.lower(), jar_file_name[self.dbms])
+                os.path.join("driver", self.dbms.lower(), dialect_driver_files[self.dbms])
             )
 
         except jpype.JVMNotFoundException as jvm_nfe:
